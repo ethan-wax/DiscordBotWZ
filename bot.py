6d381dd8bb559eb3b12a7a0c51a2495d4fafc9fa
Ethan Waxman <ejw99@cornell.edu>
2021-03-07 12:01:47 -0500

    Create the get_stat function
    - Created a function to retrieve the stats for a given player from the
      warzone API

--- bot.py
+++ bot.py
@@ -1,17 +1,34 @@
 import discord
-import os
+import vals
 import requests
+import json
 
 client = discord.Client()
 
+def get_stat(user, platform):
+  """
+  Retrieves the stats of the given player from the warzone API
+  """
+  url = 'https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/{0}/gamer/{1}/profile/type/mp'.format(platform, user)
+
+  headers = {'Cookie':COOKIE}
+
+  response = requests.get(url, headers=headers)
+  json_data = json.loads(response.text)
+  return json_data['data']['username']
+
+
 @client.event
 async def on_ready():
-  print('The warzone bot is now online as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-    if message.content.startswith('!wz'):
-      print('The warzone bot is still being created.')
+    if message.content.startswith('$stat'):
+      args = message.content.split()
+      await message.channel.send(get_stat(args[1],args[2]))
+
+client.run(os.getenv(TOKEN))
